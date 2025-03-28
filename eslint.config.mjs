3c39f37d555b3648bc37e9ae9599878cb12f9f89
Krittin Tragunejindarat <t.tintinnkt@gmail.com>
2025-03-29 03:18:31 +0700

    feat: add dependency next-auth, axios, tanstack, shadcn, eslint ,prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import unicorn from "eslint-plugin-unicorn";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,60 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  prettierRecommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { ignores: [".next/*", "node_modules/*", "build/*"] },
+  {
+    plugins: { unicorn },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-expressions": "off",
+
+      "@typescript-eslint/array-type": [
+        "warn",
+        {
+          default: "generic",
+          readonly: "generic",
+        },
+      ],
+
+      "@typescript-eslint/consistent-type-definitions": ["warn", "interface"],
+
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+        },
+      ],
+    },
+    "@typescript-eslint/no-empty-interface": "warn",
+
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "TSTypeAliasDeclaration[typeAnnotation.type='TSIntersectionType']",
+        message: "Use interfaces instead of type aliases for intersections.",
+      },
+    ],
+  },
+  {
+    files: ["**/*.d.ts"],
+
+    rules: {
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "TSEnumDeclaration",
+          message:
+            "Enums are not allowed in declaration files. Use a union type or alternative approach.",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
